5567435585b7ad7571925062f609ed5851bca983
shamimHrazu <hsrazu001@gmail.com>
2021-08-27 16:35:52 +0600

    creating record from code by getting input to wizard

--- wizard/create_appointment.py
+++ wizard/create_appointment.py
@@ -4,12 +4,15 @@ from odoo import api,models, fields , _
 class CreateAppointmentWizard(models.TransientModel):
     _name = "create.appointment.wizard"
     _description = "Create Appointment Wizard"
-
-    name = fields.Char(string="Appointment for patient", required = True)
-    patient_id = fields.Many2one('hospital.patient' , string= "patient id", tracking = True)
+    appointment_date = fields.Date(string="Appointment Date & Time ")
+    patient_id = fields.Many2one('hospital.patient' , string= "patient id", tracking = True, required = True)
     state = fields.Selection([
                               ('confirm', 'Confirmed'),],
                              string="status", tracking=True)
 
-    def action_button_confirm(self):
-        self.state = 'confirm'
+    def action_appointment_confirm(self):
+        vals = {
+            'patient_id' : self.patient_id.id ,
+            'date_appointment' : self.appointment_date,
+        }
+        self.env['hospital.patient.appointment'].create(vals)
